037e5369510e9f7736c54d77f60666a87e98bd7e
smach <smachlis@gmail.com>
2014-02-28 13:45:44 -0500

    Create InstallPackagesNICAR2014.R

---
+++ InstallPackagesNICAR2014.R
@@ -0,0 +1,15 @@
+# Run this script to install all packages used in the NICAR 2014 Introduction to R session
+
+install.packages("lattice")
+install.packages("psych")
+install.packages("plyr")
+install.packages("dplyr")
+install.packages("choroplethr")
+
+install.packages("rCharts")
+install.packages("doBy")
+install.packages("ggplot2")
+install.packages("quantmod")
+install.packages("devtools")
+require(devtools)
+install_github('rCharts', 'ramnathv')
